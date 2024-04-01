7b460603ac28f72f64a7a89b153fd13d9cb51354
kwanelexavi <kwanelexavi@gmail.com>
2024-04-01 08:43:22 -0700

    Status of your API

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -102,6 +102,6 @@ class DBStorage:
 
         return count
 
-     def close(self):
+    def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
